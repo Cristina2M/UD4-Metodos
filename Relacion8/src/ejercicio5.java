10c412843a08e5ec70c08c661f543cda777db78b
Cristina2M <cmormar2509@g.educaand.es>
2024-11-28 19:00:22 +0000

    ej5

---
+++ Relacion8/src/ejercicio5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class ejercicio5 {
+    //5. Define la función concatena con la siguiente cabecera:
+    //public static int[] concatena(int[] a, int[] b)
+    //Esta función toma dos arrays como parámetros y devuelve un array que es el resultado de concatenar
+    //ambos. Por ej. si a = { 8, 9, 0 } y b = { 1, 2, 3 }, concatena(a, b) devuelve { 8, 9, 0, 1, 2, 3 }.
+    
+
+
+
+
+
+public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+
+    
+
+}
+}
