22e88114279c84597e9c2c3c698fca33b3a6f95d
Cristina2M <cmormar2509@g.educaand.es>
2024-11-28 13:15:06 +0000

    ej2Terminado

--- Relacion8/src/ejercicio2.java
+++ Relacion8/src/ejercicio2.java
@@ -1,7 +1,40 @@
+import java.util.Scanner;
 public class ejercicio2 {
+    // 2. Crea una función con la siguiente cabecera:
+    // public String convierteEnPalabras(int n)
+    // Esta función convierte los dígitos del número n en las correspondientes
+    // palabras y lo devuelve todo en
+    // una cadena de caracteres. Por ejemplo, el 470213 convertido a palabras sería:
+    // cuatro, siete, cero, dos, uno, tres
+    // Utiliza esta función en un programa para comprobar que funciona bien. Desde
+    // la función no se debe
+    // mostrar nada por pantalla, solo se debe usar print desde el programa
+    // principal. Fíjate que hay una
+    // coma detrás de cada palabra salvo al final.
+
+    static String convierteEnPalabras(int n){
+        String[] nombres_numeros = {"cero","uno","dos","tres","cuatro","cinco","seis","siete","ocho","nueve"};
+            String cadena = String.valueOf(n);
+            String nombres = "";
     
+            for (int i = 0; i < cadena.length(); i++) {
+                int posicion = cadena.charAt(i) - 48;
+                nombres = nombres + nombres_numeros[posicion];//decimos que vaya añadiendo al valor nombres el string en esa celda del arraid
+
+                if (i < cadena.length()-1) {//restamos 1 para que no añada una , extra al final
+                 nombres = nombres + ", ";
+                }
+            }
+        return nombres;
+    }
 
 
+    public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+        int n;
 
-    
+    System.out.println("Introduce un número:");
+    n = entrada.nextInt();
+    System.out.println("Los números en palabras serían: " + convierteEnPalabras(n));
+    }
 }
