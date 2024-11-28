8ae30206002da2ba9f22a67d6a748b897e3034f6
Cristina2M <cmormar2509@g.educaand.es>
2024-11-28 12:38:46 +0000

    ej1Comentado

--- Relacion8/src/ejercicio1.java
+++ Relacion8/src/ejercicio1.java
@@ -3,17 +3,17 @@ public class ejercicio1 {
 
 
     static String convierteEnPalotes(int n){
-        String cadena = String.valueOf(n);
+        String cadena = String.valueOf(n);//funcion para que convierta un entero a cadena
         String palos = "";
 
         for (int i = 0; i < cadena.length(); i++) {
-            int num = cadena.charAt(i) - 48;
+            int num = cadena.charAt(i) - 48;//se le resta 48 porque el valor que detecta en el 0 es 48 segun la tabla ASCI
 
             for (int j = 0; j < num; j++) {
                 palos = palos + "|";
             }
-            
-            if (i < cadena.length()-1) {
+
+            if (i < cadena.length()-1) {//se le resta 1 para que no añada un - de más
                 palos = palos + "-";
             }
         }
@@ -27,6 +27,5 @@ public class ejercicio1 {
     System.out.println("Introduce un número:");
     n = entrada.nextInt();
     System.out.println("El número introducido cambiado a palos seria: " + convierteEnPalotes(n));
-
     }
 }
