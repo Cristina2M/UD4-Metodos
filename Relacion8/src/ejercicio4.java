aff45d214388430eb7475bc72690d3656f89be4a
Cristina2M <cmormar2509@g.educaand.es>
2024-11-28 19:04:34 +0000

    ej4

--- Relacion8/src/ejercicio4.java
+++ Relacion8/src/ejercicio4.java
@@ -1,19 +1,17 @@
 import java.util.Scanner;
 public class ejercicio4 {
-    //4. Define la función convierteArrayEnString con la siguiente cabecera:
-//public static String convierteArrayEnString(int[] a)
-//Esta función toma como parámetro un array que contiene números y devuelve una cadena de
-//caracteres con esos números. Por ejemplo, si a = { }, convierteArrayEnString(a) devuelve “”; si a = { 8 },
-//convierteArrayEnString(a) devuelve “8”; si a = { 6, 2, 5, 0, 1 }, convierteArrayEnString(a) devuelve
-//“62501”.
+    // 4. Define la función convierteArrayEnString con la siguiente cabecera:
+    // public static String convierteArrayEnString(int[] a)
+    // Esta función toma como parámetro un array que contiene números y devuelve una
+    // cadena de
+    // caracteres con esos números. Por ejemplo, si a = { },
+    // convierteArrayEnString(a) devuelve “”; si a = { 8 },
+    // convierteArrayEnString(a) devuelve “8”; si a = { 6, 2, 5, 0, 1 },
+    // convierteArrayEnString(a) devuelve
+    // “62501”.
 
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
 
-
-public static void main(String[] args) {
-    Scanner entrada = new Scanner(System.in);
-
-
-
-    
-}
+    }
 }
