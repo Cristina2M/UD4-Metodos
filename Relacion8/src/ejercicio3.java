80e5cb27e48b75882eda82107a38e8dacb4b37ba
Cristina2M <cmormar2509@g.educaand.es>
2024-12-03 08:43:08 +0000

    Ej3 Comentado

--- Relacion8/src/ejercicio3.java
+++ Relacion8/src/ejercicio3.java
@@ -10,25 +10,20 @@ public class ejercicio3 {
     // que pinte una línea con el carácter especificado.
 
     static void triangulo (int altura){
-        int resto=altura;//para restarle 1 y que cada vez escriba un * menos
-        for (int i = 0; i < altura; i++) {
-            for (int j = resto; j > 0 ; j--) {
+        int resto=altura;//para restarle 1 y que cada vez escriba un * menos, añadimos otra variable
+        for (int i = 0; i < altura; i++) {//hacemos bucle para el nº de lineas
+            for (int j = resto; j > 0 ; j--) {//y este para el nº de *
                 System.out.print("*");
             }
-            resto=resto-1;
-            System.out.println();
+            resto=resto-1;//le quitamos 1 para que la proxima vex escriba 1 menos
+            System.out.println();//y le añadimos un salto de linea
         }
     }
-
-
-
+ 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Introduce la altura del triángulo");
-        int altura=entrada.nextInt();
+        System.out.println("Introduce la altura del triángulo");//pedimos la altura
+        int altura=entrada.nextInt(); //guardamos el valor
         triangulo(altura);//invocamos el metodo
-
-
-
     }
 }
