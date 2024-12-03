8ed36b16372181daaaa99c62616a8ce61045b90d
Cristina2M <cmormar2509@g.educaand.es>
2024-12-03 08:42:53 +0000

    ej4Completo

--- Relacion8/src/ejercicio4.java
+++ Relacion8/src/ejercicio4.java
@@ -10,8 +10,28 @@ public class ejercicio4 {
     // convierteArrayEnString(a) devuelve
     // “62501”.
 
+
+    static String convierteArrayEnString(int[] a){
+        String numeros="";//declaro la variable donde voy a guardar los valores
+            for (int i = 0; i < a.length; i++) {//hacemos un bucle for con el tamaño del array
+                numeros= numeros + a[i]; //almaceno los valores de cada casilla
+                //tambien se puede poner como -> numeros += a[i];
+            }
+            return numeros;
+    }
+
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        System.out.println("Introduce la cantidad de valores que quieras en el array: ");
+        int tamaño = entrada.nextInt();
+        int[] a = new int[tamaño]; // inicio el array con el tamaño introducido
+        System.out.println("Introduce el valor para cada casilla de tu array: ");
 
+        for (int i = 0; i < tamaño; i++) {
+            int num = entrada.nextInt();
+            a[i] = num; //en el array a, en la casilla i, se introduce el valor
+        }
+
+        System.out.println("El numero sería: " + convierteArrayEnString(a));
     }
 }
